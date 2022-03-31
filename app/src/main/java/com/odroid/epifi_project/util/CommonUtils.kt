c49ead588016370ec1480aeed966a3860945206d
Nikhil Singh <nikhil.singh@olx.com>
2022-04-01 03:08:36 +0530

    Code cleaned for viewModel and utils

--- app/src/main/java/com/odroid/epifi_project/util/CommonUtils.kt
+++ app/src/main/java/com/odroid/epifi_project/util/CommonUtils.kt
@@ -2,10 +2,38 @@ package com.odroid.epifi_project.util
 
 import android.content.Context
 import android.widget.Toast
+import com.odroid.epifi_project.entity.Constants
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 object CommonUtils {
 
     fun showToast(context: Context, toastMessage: String) {
         Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show()
+    }
+
+    fun isPanCardNumberValid(panCardNo: String?): Boolean {
+        val panCardRegex = Constants.PAN_CARD_REGEX
+        val panCardPattern: Pattern = Pattern.compile(panCardRegex)
+        val panCardMatcher: Matcher = panCardPattern.matcher(panCardNo)
+        return panCardMatcher.matches()
+    }
+
+    fun isDateOfBirthValid(
+        dateFormat: SimpleDateFormat,
+        birthDate: String
+    ): Boolean {
+        return try {
+            val dateOfBirth: Date = dateFormat.parse(birthDate)
+            val today = Date(System.currentTimeMillis() - 1000 * 60 * 60 * 24)
+            if (dateOfBirth.before(today)) {
+                return true
+            }
+            false
+        } catch (e: Exception) {
+            false
+        }
     }
 }
--- app/src/main/java/com/odroid/epifi_project/viewModel/MainActivityViewModel.kt
+++ app/src/main/java/com/odroid/epifi_project/viewModel/MainActivityViewModel.kt
@@ -6,11 +6,10 @@ import androidx.lifecycle.viewModelScope
 import com.odroid.epifi_project.entity.AccountDetails
 import com.odroid.epifi_project.entity.BirthDetails
 import com.odroid.epifi_project.entity.Constants
+import com.odroid.epifi_project.util.CommonUtils.isDateOfBirthValid
+import com.odroid.epifi_project.util.CommonUtils.isPanCardNumberValid
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
-import java.util.*
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class MainActivityViewModel : ViewModel() {
 
@@ -19,45 +18,33 @@ class MainActivityViewModel : ViewModel() {
     fun validateFields(accountDetails: AccountDetails) {
         viewModelScope.launch {
             val isFieldsValidationSuccessful = when {
-                isValidPanCardNo(accountDetails.panCardNo)
-                        && isBirthDateValid(accountDetails.birthDetails) -> true
+                checkIfPanCardNumberIsValid(accountDetails.panCardNo)
+                        && checkIfDateOfBirthIsValid(accountDetails.birthDetails) -> true
                 else -> false
             }
             validationLiveData.postValue(isFieldsValidationSuccessful)
         }
     }
 
-    private fun isValidPanCardNo(panCardNo: String?): Boolean {
+    private fun checkIfPanCardNumberIsValid(panCardNo: String?): Boolean {
         panCardNo?.let {
             if (it.length < Constants.PAN_CARD_LENGTH) {
                 return false
             }
         } ?: return false
-        val panCardRegex = Constants.PAN_CARD_REGEX
-        val panCardPattern: Pattern = Pattern.compile(panCardRegex)
-        val panCardMatcher: Matcher = panCardPattern.matcher(panCardNo)
-        return panCardMatcher.matches()
+        return isPanCardNumberValid(panCardNo)
     }
 
-    private fun isBirthDateValid(birthDetails: BirthDetails): Boolean {
+    private fun checkIfDateOfBirthIsValid(birthDetails: BirthDetails): Boolean {
         val date = birthDetails.birthDate
         val month = birthDetails.birthMonth
         val year = birthDetails.birthYear
         if (year.length < Constants.YEAR_LENGTH)
             return false
-        val sdf = SimpleDateFormat(Constants.DATE_FORMAT)
+        val dateFormat = SimpleDateFormat(Constants.DATE_FORMAT)
         val birthDate = date.plus("/").plus(month).plus("/")
             .plus(year)
-        sdf.isLenient = false
-        return try {
-            val dateOfBirth: Date = sdf.parse(birthDate)
-            val today = Date(System.currentTimeMillis() - 1000 * 60 * 60 * 24)
-            if (dateOfBirth.before(today)) {
-                return true
-            }
-            false
-        } catch (e: Exception) {
-            false
-        }
+        dateFormat.isLenient = false
+        return isDateOfBirthValid(dateFormat, birthDate)
     }
 }
